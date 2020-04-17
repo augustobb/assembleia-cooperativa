cebbe4e4b23f3784c717a2d72f6d775da92574bd
Augusto Bernd <augustobb@msn.com>
2020-04-17 06:57:33 -0300

    Swagger, configs banco

--- src/main/java/com/cooperativa/assembleia/api/controller/AssociadoAPI.java
+++ src/main/java/com/cooperativa/assembleia/api/controller/AssociadoAPI.java
@@ -2,6 +2,8 @@ package com.cooperativa.assembleia.api.controller;
 
 import com.cooperativa.assembleia.api.request.AssociadoRequest;
 import com.cooperativa.assembleia.api.response.AssociadoResponse;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,14 +11,21 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RequestMapping("/associados")
+@RestController
 public interface AssociadoAPI {
 
     @GetMapping
+    @ApiOperation(value = "Retorna todos os associados", response = AssociadoResponse.class, responseContainer = "List")
     ResponseEntity<List<AssociadoResponse>> buscarTodos();
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "Busca associado pelo identificador", response = AssociadoResponse.class)
     ResponseEntity<AssociadoResponse> buscarPorId(@PathVariable Long id);
 
     @PostMapping
-    ResponseEntity<AssociadoResponse> cadastrar(@RequestBody @Valid AssociadoRequest associado);
+    @ApiOperation(value = "Cadastra um novo associado, retornando associado cadastrado", response = AssociadoResponse.class)
+    ResponseEntity<AssociadoResponse> cadastrar(
+            @RequestBody
+            @ApiParam(value="Corpo de cadastro do associado", required=true)
+            @Valid AssociadoRequest associado);
 }
--- src/main/java/com/cooperativa/assembleia/api/controller/PautaAPI.java
+++ src/main/java/com/cooperativa/assembleia/api/controller/PautaAPI.java
@@ -4,6 +4,8 @@ import com.cooperativa.assembleia.api.request.PautaRequest;
 import com.cooperativa.assembleia.api.request.VotoRequest;
 import com.cooperativa.assembleia.api.response.PautaResponse;
 import com.cooperativa.assembleia.api.response.ResultadoResponse;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -17,22 +19,32 @@ import java.util.List;
 public interface PautaAPI {
 
     @GetMapping
+    @ApiOperation(value = "Retorna todas as pautas", response = PautaResponse.class, responseContainer = "List")
     ResponseEntity<List<PautaResponse>> buscarTodas();
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "Busca pauta pelo identificador", response = PautaResponse.class)
     ResponseEntity<PautaResponse> buscarPorId(@PathVariable Long id);
 
     @PostMapping
-    ResponseEntity<PautaResponse> incluir(@RequestBody @Valid PautaRequest pauta);
+    @ApiOperation(value = "Inclui uma pauta, retornando pauta criada", response = PautaResponse.class)
+    ResponseEntity<PautaResponse> incluir(
+            @RequestBody @Valid @ApiParam(value="Corpo da pauta", required=true) PautaRequest pauta);
 
     @PostMapping("/{id}/abrir-sessao")
-    ResponseEntity<Void> abrirSessao(
-            @PathVariable Long id,
-            @Min(value = 10, message = "Duração mínima: 10s") @RequestParam(defaultValue="60") Long segundosDuracao);
+    @ApiOperation(value = "Abre sessão para a pauta")
+    ResponseEntity<Void> abrirSessao(@PathVariable Long id,
+            @Min(value = 10, message = "Duração mínima: 10s")
+            @ApiParam(value = "Duração da pauta, em segundos", defaultValue = "60")
+            @RequestParam(defaultValue="60") Long segundosDuracao);
 
     @PostMapping("/{id}/votar")
-    ResponseEntity<Void> votar(@PathVariable Long id, @Valid @RequestBody VotoRequest voto);
+    @ApiOperation(value = "Registra voto para pauta")
+    ResponseEntity<Void> votar(
+            @PathVariable Long id,
+            @Valid @RequestBody @ApiParam(value="Corpo do voto", required=true) VotoRequest voto);
 
     @GetMapping("/{id}/resultados")
+    @ApiOperation(value = "Obtém resultados da pauta", response = ResultadoResponse.class)
     ResponseEntity<ResultadoResponse> buscarResultadoPauta(@PathVariable Long id);
 }
